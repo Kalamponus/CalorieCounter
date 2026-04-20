e53849c93b0fd2082e14f94d77fc5cb1bfebebcd
Kalamponus <m.maiorov@inbox.ru>
2026-04-20 21:48:09 +0300

    Added basic logging

--- Program.cs
+++ Program.cs
@@ -1,4 +1,15 @@
+using Serilog;
+
+Log.Logger = new LoggerConfiguration()
+    .Enrich.FromLogContext()
+    .WriteTo.Console()
+    .CreateBootstrapLogger();
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Host.UseSerilog((context, configuration) =>
+    configuration.ReadFrom.Configuration(context.Configuration));
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
