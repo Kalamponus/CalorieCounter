ae62525b810c8ac90dc39c00cd7b4bd4808b09bb
Kalamponus <m.maiorov@inbox.ru>
2026-06-02 22:05:53 +0300

    domain result WIP

---
+++ CalorieCounter/Domain/DomainErrors/DomainError.cs
@@ -0,0 +1,12 @@
+﻿namespace CalorieCounter.Domain.DomainErrors
+{
+    public readonly record struct DomainError
+    {
+        public string Message { get; init; }
+
+        public DomainError(string message = "Business rules are not satisfied by operation's data.")
+        {
+            Message = message;
+        }
+    }
+}
---
+++ CalorieCounter/Domain/DomainResults/DomainResult.cs
@@ -0,0 +1,20 @@
+﻿using CalorieCounter.Domain.DomainErrors;
+
+namespace CalorieCounter.Domain.DomainResults
+{
+    public class DomainResult
+    {
+        public bool HasErrors => Errors.Any();
+        public List<DomainError> Errors { get; private set; } = [];
+
+        public void AddError(DomainError error)
+        {
+            Errors.Add(error);
+        }
+
+        public void AddError(string message)
+        {
+            Errors.Add(new DomainError(message));
+        }
+    }
+}
