59eca24a858424f6979355a2bd0f4ddebf5c247e
Kalamponus <m.maiorov@inbox.ru>
2026-06-09 21:07:28 +0300

    Added user dto validator

---
+++ CalorieCounter/Domain/Validators/UserValidator.cs
@@ -0,0 +1,18 @@
+﻿using CalorieCounter.Application.DTO;
+using FluentValidation;
+
+namespace CalorieCounter.Domain.Validators
+{
+    public class UserValidator : AbstractValidator<UserDto>
+    {
+        public UserValidator()
+        {
+            RuleFor(user => user.Id).NotEqual(Guid.Empty);
+            RuleFor(user => user.Name).NotNull().NotEmpty();
+            RuleFor(user => user.Age).NotEmpty();
+            RuleFor(user => user.Height).NotEmpty();
+            RuleFor(user => user.Weight).NotEmpty();
+            RuleFor(user => user.TargetWeight).NotEmpty();
+        }
+    }
+}
--- CalorieCounter/Program.cs
+++ CalorieCounter/Program.cs
@@ -2,6 +2,7 @@
 using Serilog;
 using CalorieCounter.Application;
 using CalorieCounter.Persistence;
+using FluentValidation;
 
 Log.Logger = new LoggerConfiguration()
     .Enrich.FromLogContext()
@@ -17,6 +18,7 @@ builder.Services.AddProblemDetails();
 
 builder.Services.AddApplication();
 builder.Services.AddPersistence(builder.Configuration.GetConnectionString("MainDB") ?? throw new Exception("Connection string 'MainDB' not found."));
+builder.Services.AddValidatorsFromAssemblyContaining<Program>();
 
 var app = builder.Build();
 
