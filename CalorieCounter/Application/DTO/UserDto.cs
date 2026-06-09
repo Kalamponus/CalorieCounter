ce15f32c0e447dce042e287bf46a19d6d5501875
Kalamponus <m.maiorov@inbox.ru>
2026-06-09 19:49:57 +0300

    Added user dto

---
+++ CalorieCounter/Application/DTO/UserDto.cs
@@ -0,0 +1,15 @@
+﻿using CalorieCounter.Domain.AggregatesModels.UserAggregate;
+
+namespace CalorieCounter.Application.DTO
+{
+    public record UserDto
+    {
+        public required Guid Id { get; set; }
+        public required string Name { get; set; }
+        public required int Age { get; set; }
+        public required Gender Gender { get; set; }
+        public required float Weight { get; set; }
+        public required float Height { get; set; }
+        public required float TargetWeight { get; set; }
+    }
+}
